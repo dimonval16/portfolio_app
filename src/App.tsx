5148a1dc90b1fda17207aae4cbe5ec3c6916f262
Dmytro Volynskyi <dimonval16@gmail.com>
2020-07-18 12:55:23 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css';
+
+const App: FC = () => {
+    return (
+        <div className={'app'}>
+            Hello world
+        </div>
+    );
+};
+
+export default App;
